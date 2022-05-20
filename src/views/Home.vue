c57899d8f6ffb8e7ad63078e977b3956facae36e
George TURKSON <georgesorho@gmail.com>
2022-05-20 11:27:35 +0000

    add cap-rover definition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,6 +24,16 @@
         <p class="lead">
           <router-link :to="displayParam.link" class="btn btn-md btn-secondary fw-bold border-white bg-white">View More</router-link>
         </p>
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
 
             <!-- <div class="dropdown" v-show="displayParam.id == 0" >
               <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
