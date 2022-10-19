92b5801278d05130f666b2fd728def21dca353fd
gtsorho <gerogesorho@gmail.com>
2022-10-19 06:42:18 +0000

    backend moved to aghub

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
 const express = require('express')
 const path = require('path')
-// const weather = require('./routes/weather')
-// const broadcast = require('./routes/broadcast') 
-const cors = require('cors');
-require('dotenv').config()
 
 const app = express()
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.use(cors({
-    origin: '*'
-}));
 
-// new
-// app.use('/api/weather', weather)
-// app.use('/api/broadcast', broadcast)
-
-// app.use('/', serveStatic(path.join(__dirname,'/dist')))
-
-// app.get(/.*/, function(req, res){
-//     res.sendFile(path.join(__dirname, 'dist/index.html'))
-// })
-
-  
 
 const port = process.env.PORT || 3000
 app.listen(port)
