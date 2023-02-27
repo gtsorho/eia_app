32777a3ee356e658d79bca859425bc968917d8ef
gtsorho <gerogesorho@gmail.com>
2023-02-27 12:01:37 +0000

    error checks

--- src/views/tools.vue
+++ src/views/tools.vue
@@ -195,7 +195,7 @@ export default {
           const d = new Date();
           d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
           let expires = "expires=" + d.toUTCString();
-          document.cookie = cname + "=" + cvalue + ";"+"domain=http://weathr.miphost.com;"+ expires + ";path=/";
+          document.cookie = cname + "=" + cvalue + ";"+ expires + ";path=/";
         },
     },
     created(){
